a3a6caadbd0385bb77974c407be42f2434c1ac58
tam47-c <t47amr@gmail.com>
2024-11-25 01:15:03 +0300

    add multiple c++ exercises

---
+++ Exercise1.cpp
@@ -0,0 +1,11 @@
+//Title: Hello World
+//Description: Write a C++ program that prints "Hello, World!" to the console.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout<<"Hello World";
+  
+  return 0;
+}
